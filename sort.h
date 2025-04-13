7295ee3a080333890e81fb04be1eb2556a4962d4
Juan Pablo <jpbriceno2001@gmail.com>
2025-04-13 16:47:59 +0000

    1-insertion_sort_list

--- sort.h
+++ sort.h
@@ -3,16 +3,6 @@
 
 #include <stddef.h>
 
-/* Provided printing functions */
-void print_array(const int *array, size_t size);
-void print_list(const struct listint_s *list);
-
-/* Sorting algorithms */
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(struct listint_s **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-
 /**
  * struct listint_s - Doubly linked list node
  * @n: Integer stored in the node
@@ -26,5 +16,14 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-#endif /* SORT_H */
+/* Provided printing functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
+/* Sorting algorithms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif /* SORT_H */
