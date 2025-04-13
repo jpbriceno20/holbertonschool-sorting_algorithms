75e01d180318d067b2656dc63fffcba1f5e36ebe
Juan Pablo <jpbriceno2001@gmail.com>
2025-04-13 17:10:47 +0000

    C - Sorting algorithms & Big O

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,15 +1,15 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array using Selection sort
- * @array: Pointer to the array
+ * selection_sort - Sorts an array of integers using Selection sort
+ * @array: Pointer to the array of integers
  * @size: Number of elements in the array
  *
  * Description: Prints the array after each swap
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
+	size_t i, j, min_index;
 	int temp;
 
 	if (!array || size < 2)
@@ -17,17 +17,19 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
+		min_index = i;
+
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min])
-				min = j;
+			if (array[j] < array[min_index])
+				min_index = j;
 		}
-		if (min != i)
+
+		if (min_index != i)
 		{
 			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
+			array[i] = array[min_index];
+			array[min_index] = temp;
 			print_array(array, size);
 		}
 	}
