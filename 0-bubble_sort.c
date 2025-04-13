88a06e3bd00a0a7e12dc973675b40f71650f640d
Juan Pablo <jpbriceno2001@gmail.com>
2025-04-13 16:57:50 +0000

    C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,27 @@
-#include "sort.h"
-#include <stdio.h>
+#ifndef SORT_H
+#define SORT_H
 
-void bubble_sort(int *array, size_t size)
+#include <stddef.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
 {
-    size_t i, n;
-    int temp, swapped;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
 
-    if (!array || size < 2)
-        return;
+/* Function Prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
-    for (n = size; n > 1; n--)
-    {
-        swapped = 0;
-        for (i = 0; i < n - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                print_array(array, size);
-                swapped = 1;
-            }
-        }
-        if (!swapped)
-            break;
-    }
-}
-
+#endif /* SORT_H */
